3b7050f9d725178805c9d1534aa42fc7f5baad75
sidney <sidney.bernardin@gmail.com>
2024-02-13 11:51:38 -0500

    Added comments.

--- web/alpine/pdf-viewer.ts
+++ web/alpine/pdf-viewer.ts
@@ -22,23 +22,27 @@ window.alpine.data("pdfViewer", (pdfURL: string, pdfID: string) => ({
             filterKey: () => true,
         });
 
-        this.pdfViewer = new PDFViewer(pdfURL, viewerContainerElem, async () => {
+        // Create a PDFViewerController and have it initialize it's fields..
+        this.pdfViewer = new PDFViewerController(pdfURL, viewerContainerElem, async () => {
             await this.pdfViewer.renderPage(1);
             htmx.trigger(initFormElem, "submit", null);
         });
 
+        // Reinitialize PDF fields when the current page changes.
         this.$watch("currentPage", async (newVal: number) => {
             await this.pdfViewer.renderPage(newVal);
             htmx.trigger(initFormElem, "submit", null);
         });
 
+        // Reinitialize PDF fields when the current tab changes.
         this.$watch("currentDynamicTab", (newVal: string) => {
             if (newVal === pdfID) htmx.trigger(initFormElem, "submit", null);
         });
     },
 }));
 
-function PDFViewer(pdfURL: string, viewerContainerElem: HTMLDivElement, cb?: () => void) {
+// A wrapper for the PDF.js package.
+function PDFViewerController(pdfURL: string, viewerContainerElem: HTMLDivElement, cb?: () => void) {
     getDocument(pdfURL).promise.then(async (res) => {
         this.doc = res;
         this.pageView = new PDFPageView({
--- web/htmx/index.ts
+++ web/htmx/index.ts
@@ -1,22 +1,21 @@
 window.htmx = require("htmx.org");
 import "/static/ws.js";
 
+// TODO: Improve error handling.
 document.body.addEventListener("htmx:responseError", (e: CustomEvent) => {
     alert(e.detail.xhr.response);
 });
 
 document.body.addEventListener("htmx:wsConfigSend", (e: CustomEvent) => {
-    switch (e.detail.parameters["TYPE"]) {
-        case "UPDATE_PDF_PAGE":
-            e.detail.parameters["pdf_fields"] = {};
+    if (e.detail.parameters["TYPE"] === "UPDATE_PDF_PAGE") {
+        e.detail.parameters["pdf_fields"] = {};
 
-            for (const [k, v] of Object.entries(e.detail.parameters)) {
-                if (k.startsWith("PDF_")) {
-                    e.detail.parameters["pdf_fields"][k] = v;
-                    delete e.detail.parameters[k];
-                }
+        // Moves the message's PDF fields into the nested-object, pdf_fields.
+        for (const [k, v] of Object.entries(e.detail.parameters)) {
+            if (k.startsWith("PDF_")) {
+                e.detail.parameters["pdf_fields"][k] = v;
+                delete e.detail.parameters[k];
             }
-
-            break;
+        }
     }
 });
